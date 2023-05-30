9d603de90b214b3e201035237ecedec7cfa3a2fa
oldstreetloft <consulting@oldstreetloft.io>
2023-05-30 16:20:29 -0400

    Added apk

--- Scripts/readme_gen.sh
+++ Scripts/readme_gen.sh
@@ -3,7 +3,7 @@
 main() {
     echo "# Android Apk Files"
     url="https://github.com/oldstreetloft/android/raw/main/Apks/"
-    find ../Apks/ -name "*.apk" | sort | while read apkfile; do
+    find Apks/ -name "*.apk" | sort | while read apkfile; do
         apkfile=$(echo $apkfile | cut -c 10-)
         apkname=$(echo $apkfile | rev | cut -c 5- | rev)
         echo ; echo "[$apkname]($url$apkfile)" ; done
