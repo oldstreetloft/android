649e232094bdb60f68ef40c97c83e9db12135a32
oldstreetloft <consulting@oldstreetloft.io>
2023-06-04 18:45:25 -0400

    App updated.

--- Scripts/readme_generate.sh
+++ Scripts/readme_generate.sh
@@ -18,13 +18,13 @@ write_readme() {
         apkname=$(echo $apkfile | rev | cut -c 5- | rev)    # hyperlink display name
         printf "\n\n*[$apkname]($url$apkfile)*" ; done    # Write hyperlink
         query_ps_link
-        printf "        https://play.google.com$APP_LINK"
 }
 
 query_ps_link() {	# Query Google Playstore for the app extract app link from query
     QUERY=$(curl -s "https://play.google.com/store/search?q=$apkname&c=apps")
 	REGEX='href="/store/apps/details?id=[^"]*"'
     APP_LINK=$(echo "$QUERY" | grep -o "$REGEX" | head -n 1 | cut -c7- | sed 's/.$//')
+    printf "https://play.google.com$APP_LINK"
 }
 
 # Start execution
