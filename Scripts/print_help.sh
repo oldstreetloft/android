6e64bf79cb0902ef658940253841aa16b8384827
oldstreetloft <consulting@oldstreetloft.io>
2023-06-04 16:43:26 -0400

    App updated.

--- Scripts/print_help.sh
+++ Scripts/print_help.sh
@@ -3,7 +3,7 @@
 main() {
 printf "\nUsage:\n\nmake\n"
 grep ":" makefile | tail -n +4 | sed 's/://g' | sed 's/^/make /g'
-printf "\nDefault action:\n\nUpdates FDroid apps\nGenerates new README.md\nCommits all changes\n\n"
+printf "\nDefault action:\n\nUpdates FDroid apps, generates new README.md, and commits all changes\n\n"
 }
 
 # Start execution
