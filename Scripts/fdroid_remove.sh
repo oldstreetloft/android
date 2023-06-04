91c49946695b0cd2441c7ea517861e2dff5118f7
oldstreetloft <consulting@oldstreetloft.io>
2023-06-04 15:04:49 -0400

    Update.

--- Scripts/fdroid_remove.sh
+++ Scripts/fdroid_remove.sh
@@ -1,30 +1,20 @@
 #!/bin/bash
 
 main() {
-    # Download .apk from FDroid website using fdroid.plist as source, inform user of non-existant app
-    printf "\nDownloading .apk files from FDroid:\n\n"
-    cat "fdroid.plist" | while read APP_NAME_RAW ; do           # Itterate through each app in fdroid.plist
-        fdroid_download ; done
+        read -p "Enter FDroid query string: " APP_NAME
+        fdroid_remove | tee fdroid_new.plist ; echo
+        mv fdroid_new.plist fdroid.plist
+        rm -rf "Apks/$APP_NAME.apk"
 }
-
-fdroid_download() {
-        # Format app names
-        APP_NAME_PATH=$(echo $APP_NAME_RAW | sed 's/ //g')      # Remove whitespace for use in filename and README.md
-        APP_NAME_URL=$(echo "$APP_NAME_RAW" | tr ' ' '+')       # Spaces to plus symbols for use in URLs
-        # Fetch FDroid search results to variable
-        QUERY_FDROID=$(curl -sL "https://search.f-droid.org/?q=$APP_NAME_URL")
-        # Parse Results for app page
-        PARSE_QUERY=$(echo "$QUERY_FDROID" | grep package-header | grep -o 'href="[^"]*"' | sed 's/href="//;s/"$//' | head -n 1)
-        # Fetch app page results to a variable
-        APP_PAGE=$(curl -sL "$PARSE_QUERY")
-        # Parse app page results for URLs
-        DOWN_URL=$(echo "$APP_PAGE" | tr " " "\n" | grep .apk | tail -n +2 | grep -o '".*"' | sed 's/"//g' | head -n 1)
-        PNG_URL=$(echo "$APP_PAGE" | tr " " "\n" | grep .png | grep repo | grep content | grep -o '".*"' | sed 's/"//g' | head -n 1)
-        # Inform user about non-existant app
-        [ -z "$DOWN_URL" ] && printf "\n\nERROR: $APP_NAME_PATH failed to query on F-Droid!\n\n"
-        # Download apk to apk folder, check for success
-        printf "Downloading $APP_NAME_PATH"
-        curl -sL $DOWN_URL -o "Apks/$APP_NAME_PATH.apk" && printf " ✓\n"
+fdroid_remove() {
+        # Read file line by line
+        while IFS= read -r LINE; do
+                # Compare the line with the app name
+                if [[ "$LINE" != "$APP_NAME" ]]; then
+                        echo "$LINE"
+                fi
+        done < fdroid.plist
+        printf "# Add FDroid query above this line."
 }
 
 # Start execution
