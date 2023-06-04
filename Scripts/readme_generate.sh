e1c275ec57a5813bfc1b4258d6c9b9558d4d87bd
oldstreetloft <consulting@oldstreetloft.io>
2023-06-04 18:35:23 -0400

    App updated.

--- Scripts/readme_generate.sh
+++ Scripts/readme_generate.sh
@@ -9,27 +9,14 @@ main() {
     printf " ✓\n\n"
 }
 
-write_readme(){
+write_readme() {
     # Write title
     echo "# Android Apks"
-    # Itterate through each apk in apk folder and write corresponding hyperlinks
+    # Itterate through each app in apk folder, write corresponding hyperlinks
     find Apks/ -name "*.apk" | sort | while read apkfile ; do
         apkfile=$(echo $apkfile | cut -c 7-)                # hyperlink filename
         apkname=$(echo $apkfile | rev | cut -c 5- | rev)    # hyperlink display name
-        write_hyperlink 
-        write_ps_link ; done
-}
-
-write_hyperlink() {
-    printf "\n\n*[$apkname]($url$apkfile)*\n"              # Write hyperlink
-}
-
-write_ps_link() {	# Query Google Playstore for the app extract app link from query
-    QUERY=$(curl -s "https://play.google.com/store/search?q=$apkname&c=apps")
-	REGEX='href="/store/apps/details?id=[^"]*"'
-    APP_LINK=$(echo "$QUERY" | grep -o "$REGEX" | head -n 1 | cut -c7- | sed 's/.$//')
-    printf "https://play.google.com$APP_LINK" ; echo
-
+        printf "\n\n*[$apkname]($url$apkfile)*\n" ; done    # Write hyperlink
 }
 
 # Start execution
