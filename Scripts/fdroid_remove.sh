485a45b1a3ce91d67b8e80a8b30b0d6c663d7d2b
oldstreetloft <consulting@oldstreetloft.io>
2023-06-04 20:47:13 -0400

    App updated.

--- Scripts/fdroid_remove.sh
+++ Scripts/fdroid_remove.sh
@@ -2,19 +2,20 @@
 
 main() {
         # Read app name for removal
-        read -p "Enter FDroid query: " APP_NAME
+        read -p "Enter FDroid query: " APP_NAME_RAW
+        APP_NAME=$(echo $APP_NAME_RAW | sed 's/ //g')
         # Sort and write new plist
         fdroid_remove | sort | uniq > fdroid_new.plist
         printf "# Add FDroid query above this line." >> fdroid_new.plist
         mv fdroid_new.plist fdroid.plist
         # Remove apk
         rm -rf "Apks/$APP_NAME.apk"
-        printf "\nRemoved $APP_NAME\n"
+        printf "\nRemoved $APP_NAME_RAW\n"
 }
 
 fdroid_remove() {
         cat fdroid.plist | while read LINE; do
-                [ "$LINE" != "$APP_NAME" ] && echo "$LINE" ; done
+                [ "$LINE" != "$APP_NAME_RAW" ] && echo "$LINE" ; done
 }
 
 # Start execution
