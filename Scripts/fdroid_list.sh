8a70b0fd0a9b607653af7dba96df658a8cf1e8b5
oldstreetloft <consulting@oldstreetloft.io>
2023-06-06 16:50:37 -0400

    App updated.

--- Scripts/fdroid_list.sh
+++ Scripts/fdroid_list.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
 main() {
+        print_line
         list_apks
+        print_line
         list_fdroid
+        print_line
 }
 
 list_apks() {
         printf "\nApk folder:\n\n"
-        ls Apks/ | tr "\t" "\n"
+        ls Apks/ | tr "\t" "\n" ; echo
 }
 
 list_fdroid() {
         printf "\nFDroid package list:\n\n"
         cat fdroid.plist | sed '$d' ; echo
+}
+
+print_line() {
+    printf "================================================================================\n"
 }
 
 # Start execution
--- Scripts/print_help.sh
+++ Scripts/print_help.sh
@@ -2,32 +2,32 @@
 
 main() {
     MAKEFILE=$(cat makefile | tr "\t" "\n")
+    print_line
     print_usage
+    print_line
     print_options
+    print_line
     print_examples
+    print_line
 }
 
 print_usage() {
-    print_line
     printf "Usage:\n\n"
     printf "make <option>\n"
 }
 
 print_options() {
-    print_line
     printf "Options:\n\n"
     echo "$MAKEFILE" | grep -e ":" -e "#" | tail -n +5 | sed 's/://g' | tr "#" "\t"
 }
 
 print_examples() {
-    print_line
     printf "Examples:\n\n"
     echo "$MAKEFILE" | grep ":" | tail -n +4 | sed 's/://g' | sed 's/^/make /g'
-    print_line
 }
 
 print_line() {
-    printf "================================================================================\n"
+    printf "\n================================================================================\n\n"
 }
 
 # Start execution
