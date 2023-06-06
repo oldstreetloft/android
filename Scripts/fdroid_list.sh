85c89435df0d7941743f6998d7a0cc644a9dee69
oldstreetloft <consulting@oldstreetloft.io>
2023-06-06 14:29:51 -0400

    App updated.

--- Scripts/fdroid_list.sh
+++ Scripts/fdroid_list.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 main() {
+        list_apks
+        list_fdroid
+}
+
+list_apks() {
         printf "\nApk folder:\n\n"
         ls Apks/ | tr "\t" "\n"
+}
+
+list_fdroid() {
         printf "\nFDroid package list:\n\n"
         cat fdroid.plist | sed '$d' ; echo
 }
--- Scripts/print_help.sh
+++ Scripts/print_help.sh
@@ -2,13 +2,25 @@
 
 main() {
     MAKEFILE=$(cat makefile | tr "\t" "\n")
+    print_usage
+    print_options
+    print_examples
+    echo
+}
+
+print_usage() {
     printf "\nUsage:\n\n"
     printf "make <option>\n"
+}
+
+print_options() {
     printf "\nOptions:\n\n"
     echo "$MAKEFILE" | grep -e ":" -e "#" | tail -n +5 | sed 's/://g' | tr "#" "\t"
+}
+
+print_examples() {
     printf "\nExamples:\n\n"
     echo "$MAKEFILE" | grep ":" | tail -n +4 | sed 's/://g' | sed 's/^/make /g'
-    echo
 }
 
 # Start execution
