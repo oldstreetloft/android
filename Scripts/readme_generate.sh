4a1921af29b71c9e38c9af02d7d00cfd09a60cd0
oldstreetloft <consulting@oldstreetloft.io>
2023-06-06 14:33:10 -0400

    App updated.

--- Scripts/readme_generate.sh
+++ Scripts/readme_generate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 main() {
-    # Define main public apk folder
-    url="https://github.com/oldstreetloft/android/raw/main/Apks/"
+    # Define location of public apk folder
+    URL="https://github.com/oldstreetloft/android/raw/main/Apks/"
     # Generate README.md for each .apk in apk folder
     printf "\nWriting README.md "
     write_readme > README.md
@@ -13,10 +13,10 @@ write_readme() {
     # Write title
     printf "# Android Apks"
     # Itterate through each app in apk folder, write corresponding hyperlinks
-    find Apks/ -name "*.apk" | sort | while read apkfile ; do
-        apkfile=$(echo $apkfile | cut -c 7-)                # hyperlink filename
-        apkname=$(echo $apkfile | rev | cut -c 5- | rev)    # hyperlink display name
-        printf "\n\n*[$apkname]($url$apkfile)*" ; done      # Write hyperlink
+    find Apks/ -name "*.apk" | sort | while read APK_FILE ; do
+        APK_FILE=$(echo $APK_FILE | cut -c 7-)                # hyperlink filename
+        APK_NAME=$(echo $APK_FILE | rev | cut -c 5- | rev)    # hyperlink display name
+        printf "\n\n*[$APK_NAME]($URL$APK_FILE)*" ; done      # Write hyperlink
 }
 
 # Start execution
