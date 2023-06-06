6765fabf7d10f96a1f7af10413da133706027aa1
oldstreetloft <consulting@oldstreetloft.io>
2023-06-06 11:25:04 -0400

    App updated.

--- Scripts/fdroid_list.sh
+++ Scripts/fdroid_list.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 main() {
+        printf "\nApk list:\n"
+        ls -l Apks | awk '{print $9}'
         printf "\nFDroid package list:\n\n"
-        cat fdroid.plist | sed '$d'
-        echo
+        cat fdroid.plist | sed '$d' ; echo
 }
 
 # Start execution
