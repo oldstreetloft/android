173ab4821f5f76b9f9ce1691ed3e29b463bb97df
oldstreetloft <consulting@oldstreetloft.io>
2023-06-06 13:11:30 -0400

    App updated.

--- Scripts/print_help.sh
+++ Scripts/print_help.sh
@@ -4,7 +4,7 @@ main() {
     printf "\nUsage:\n\n"
     grep ":" makefile | tail -n +4 | sed 's/://g' | sed 's/^/make /g'
     printf "\nOptions:\n\n"
-    cat makefile | tr "\t" "\n" | grep -e ":" -e "#" | tail -n +5
+    cat makefile | tr "\t" "\n" | grep -e ":" -e "#" | tail -n +5 | tr "#" "\t"
 }
 
 # Start execution
